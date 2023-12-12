2a1d62b3ca28cd74c9398dd0a08ed34e71a1dea7
taylor <brian.t.vann@gmail.com>
2023-12-12 00:26:31 -0800

    svc available

--- v0.1/gateway/src/responses.rs
+++ v0.1/gateway/src/responses.rs
@@ -22,7 +22,7 @@ impl Service<Request<IncomingBody>> for Svc {
 	type Error = hyper::http::Error;
 	type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 	
-	fn call(&self, _req: Request<IncomingBody>) -> Self::Future {
+	fn call(&self, _req: Request<IncomingBody>) -> Self::Future {	
 		Box::pin(async {
 		  response_500()
 		})
@@ -35,3 +35,4 @@ fn response_500() -> Result<Response<Full<bytes::Bytes>>, hyper::http::Error> {
 		.header(CONTENT_TYPE, HeaderValue::from_static(HTML))
 		.body(Full::new(bytes::Bytes::from(INTERNAL_SERVER_ERROR)))
 }
+
