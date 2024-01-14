e55243bc1bfe336a819ecfc982d9eeb5f2b99bfb
taylor <brian.t.vann@gmail.com>
2024-01-13 18:35:06 -0800

    format

--- v0.1/gateway/src/config.rs
+++ v0.1/gateway/src/config.rs
@@ -3,150 +3,141 @@ use std::fmt;
 use std::fs;
 use std::path;
 
+use serde::{Deserialize, Serialize};
 use serde_json;
-use serde::{Serialize, Deserialize};
-
 
 const FILEPATH_KEY_ERR: &str = "config did not include an existing key file";
 const FILEPATH_CERT_ERR: &str = "config did not include an existing cert file";
 const PARENT_NOT_FOUND_ERR: &str = "parent directory of config not found";
 
-
 pub enum ConfigError<'a> {
-	IoError(std::io::Error),
-	JsonError(serde_json::Error),
-	Error(&'a str),
+    IoError(std::io::Error),
+    JsonError(serde_json::Error),
+    Error(&'a str),
 }
 
 impl fmt::Display for ConfigError<'_> {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			ConfigError::IoError(io_error) => write!(f, "{}", io_error),
-			ConfigError::JsonError(json_error) => write!(f, "{}", json_error),
-			ConfigError::Error(error) => write!(f, "{}", error),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ConfigError::IoError(io_error) => write!(f, "{}", io_error),
+            ConfigError::JsonError(json_error) => write!(f, "{}", json_error),
+            ConfigError::Error(error) => write!(f, "{}", error),
+        }
+    }
 }
 
 pub enum ConfigParseError<'a> {
-	UriError(<http::Uri as TryFrom<String>>::Error),
-	Error(&'a str),
+    UriError(<http::Uri as TryFrom<String>>::Error),
+    Error(&'a str),
 }
 
-impl fmt::Display for ConfigParseError<'_>  {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-  	match self {
-  		ConfigParseError::UriError(json_error) => write!(f, "{}", json_error),
-  		ConfigParseError::Error(error) => write!(f, "{}", error),
-  	}
-  }
+impl fmt::Display for ConfigParseError<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ConfigParseError::UriError(json_error) => write!(f, "{}", json_error),
+            ConfigParseError::Error(error) => write!(f, "{}", error),
+        }
+    }
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct Config {
-	pub host: String,
-	pub port: u16,
-	pub key_filepath: path::PathBuf,
-	pub cert_filepath: path::PathBuf,
-	pub addresses: collections::HashMap<String, String>,
+    pub host: String,
+    pub port: u16,
+    pub key_filepath: path::PathBuf,
+    pub cert_filepath: path::PathBuf,
+    pub addresses: collections::HashMap<String, String>,
 }
 
-
 pub fn from_filepath(filepath: &path::PathBuf) -> Result<Config, ConfigError> {
-	// get position relative to working directory
-	let config_pathbuff = match filepath.canonicalize() {
-		Ok(pb) => pb,
-		Err(e) => return Err(ConfigError::IoError(e)),
-	};
+    // get position relative to working directory
+    let config_pathbuff = match filepath.canonicalize() {
+        Ok(pb) => pb,
+        Err(e) => return Err(ConfigError::IoError(e)),
+    };
 
-	let parent_dir = match config_pathbuff.parent() {
-		Some(p) => p.to_path_buf(),
-		_ => return Err(ConfigError::Error(PARENT_NOT_FOUND_ERR)),
-	};
+    let parent_dir = match config_pathbuff.parent() {
+        Some(p) => p.to_path_buf(),
+        _ => return Err(ConfigError::Error(PARENT_NOT_FOUND_ERR)),
+    };
 
-	// build json conifg
-	let json_as_str = match fs::File::open(&config_pathbuff) {
-		Ok(r) => r,
-		Err(e) => return Err(ConfigError::IoError(e)),
-	};
+    // build json conifg
+    let json_as_str = match fs::File::open(&config_pathbuff) {
+        Ok(r) => r,
+        Err(e) => return Err(ConfigError::IoError(e)),
+    };
 
-	let config: Config = match serde_json::from_reader(&json_as_str) {
-		Ok(j) => j,
-		Err(e) => return Err(ConfigError::JsonError(e)),
-	};
+    let config: Config = match serde_json::from_reader(&json_as_str) {
+        Ok(j) => j,
+        Err(e) => return Err(ConfigError::JsonError(e)),
+    };
 
-	// create key and cert with absolute filepaths from client config
-	let key = match combine_pathbuf(
-		&parent_dir,
-		&config.key_filepath,
-	) {
-		Ok(j) => j,
-		Err(e) => return Err(ConfigError::IoError(e)),
-	};
-	if key.is_dir() {
-		return Err(ConfigError::Error(FILEPATH_KEY_ERR));
-	}
+    // create key and cert with absolute filepaths from client config
+    let key = match combine_pathbuf(&parent_dir, &config.key_filepath) {
+        Ok(j) => j,
+        Err(e) => return Err(ConfigError::IoError(e)),
+    };
+    if key.is_dir() {
+        return Err(ConfigError::Error(FILEPATH_KEY_ERR));
+    }
 
-	let cert = match combine_pathbuf(
-			&parent_dir,
-			&config.cert_filepath,
-	) {
-		Ok(j) => j,
-		Err(e) => return Err(ConfigError::IoError(e)),
-	};
-	if cert.is_dir() {
-		return Err(ConfigError::Error(FILEPATH_CERT_ERR));
-	}
+    let cert = match combine_pathbuf(&parent_dir, &config.cert_filepath) {
+        Ok(j) => j,
+        Err(e) => return Err(ConfigError::IoError(e)),
+    };
+    if cert.is_dir() {
+        return Err(ConfigError::Error(FILEPATH_CERT_ERR));
+    }
 
-	Ok(Config {
-		host: config.host,
-		port: config.port,
-		key_filepath: key,
-		cert_filepath: cert,
-		addresses: config.addresses,
-	})
+    Ok(Config {
+        host: config.host,
+        port: config.port,
+        key_filepath: key,
+        cert_filepath: cert,
+        addresses: config.addresses,
+    })
 }
 
 fn combine_pathbuf(
-	base_dir: &path::PathBuf,
-	filepath: &path::PathBuf,
+    base_dir: &path::PathBuf,
+    filepath: &path::PathBuf,
 ) -> Result<path::PathBuf, std::io::Error> {
-	base_dir.join(filepath).canonicalize()
+    base_dir.join(filepath).canonicalize()
 }
 
 /*
-	create_address_map iterates config.addresses and creates a map of
-	destination URIs indexed by a URI host.
-	ie: Map<example.com, http://some_address:6789>
-	
-	If a URI fails to parse, the entire operation fails
+    create_address_map iterates config.addresses and creates a map of
+    destination URIs indexed by a URI host.
+    ie: Map<example.com, http://some_address:6789>
+
+    If a URI fails to parse, the entire operation fails
 */
 pub fn create_address_map(
-	config: &Config,
-) -> Result<
-	collections::HashMap::<String, http::Uri>,
-	ConfigParseError,
-> {
-  let mut hashmap = collections::HashMap::<String, http::Uri>::new();
-  for (index, value) in &config.addresses {
-  	let index_uri = match http::Uri::try_from(index) {
-  		Ok(uri) => uri,
-  		Err(e) => return Err(ConfigParseError::UriError(e)),
-  	};
-  	
-  	let host = match index_uri.host() {
-			Some(uri) => uri,
-			_ => return Err(ConfigParseError::Error("could not find host from addresses")),
-		};
-		
-  	let dest_uri = match http::Uri::try_from(value) {
-  		Ok(uri) => uri,
-  		Err(e) => return Err(ConfigParseError::UriError(e)),
-  	};
-  	
-  	hashmap.insert(host.to_string(), dest_uri);
-  }
-  
-  Ok(hashmap)
-}
+    config: &Config,
+) -> Result<collections::HashMap<String, http::Uri>, ConfigParseError> {
+    let mut hashmap = collections::HashMap::<String, http::Uri>::new();
+    for (index, value) in &config.addresses {
+        let index_uri = match http::Uri::try_from(index) {
+            Ok(uri) => uri,
+            Err(e) => return Err(ConfigParseError::UriError(e)),
+        };
 
+        let host = match index_uri.host() {
+            Some(uri) => uri,
+            _ => {
+                return Err(ConfigParseError::Error(
+                    "could not find host from addresses",
+                ))
+            }
+        };
+
+        let dest_uri = match http::Uri::try_from(value) {
+            Ok(uri) => uri,
+            Err(e) => return Err(ConfigParseError::UriError(e)),
+        };
+
+        hashmap.insert(host.to_string(), dest_uri);
+    }
+
+    Ok(hashmap)
+}
--- v0.1/gateway/src/main.rs
+++ v0.1/gateway/src/main.rs
@@ -4,89 +4,87 @@ use std::sync::Arc;
 
 use hyper_util::rt::{TokioExecutor, TokioIo};
 use hyper_util::server::conn::auto::Builder;
-use native_tls::{Identity};
+use native_tls::Identity;
 use tokio::fs;
 use tokio::net::TcpListener;
 
 mod config;
 mod responses;
 
-
-
 #[tokio::main]
 async fn main() {
-	// create config
-  let args = match env::args().nth(1) {
-      Some(a) => path::PathBuf::from(a),
-      None => return println!("argument error:\nconfig params not found."),
-  };
-  let config = match config::from_filepath(&args) {
-      Ok(c) => c,
-      Err(e) => return println!("configuration error:\n{}", e),
-  };
-
-	// get addresses
-  let host_address = config.host.clone() + ":" + &config.port.to_string();
-  let addresses = match config::create_address_map(&config) {
-  	Ok(addrs) => addrs,
-  	Err(e) => return println!("address map error:\n{}", e),
-  };
-  let addresses_arc = Arc::new(addresses);
-
-  // tls cert and keys
-  let cert = match fs::read(&config.cert_filepath).await {
-  	Ok(f) => f,
-  	Err(e) => return println!("cert error:\n{}", e),
-  };
-  let key = match fs::read(&config.key_filepath).await {
-  	Ok(f) => f,
-  	Err(e) => return println!("cert error:\n{}", e),
-  };
-  let pkcs8 = match Identity::from_pkcs8(&cert, &key) {
-  	Ok(pk) => pk,
-  	Err(e) => return println!("cert error:\n{}", e),
-  };
-
-  // bind tcp listeners
-  let listener = match TcpListener::bind(host_address).await {
-  	Ok(l) => l,
-  	Err(e) => return println!("tcp listener error:\n{}", e),
-  };
-	let tls_acceptor = match native_tls::TlsAcceptor::builder(pkcs8)
-		.build() {
-			Ok(native_acceptor) => tokio_native_tls::TlsAcceptor::from(native_acceptor),
-			Err(e) => return println!("native_acceptor error:\n{}", e),
-	};
-
-	// sever loop
-  loop {
-  	// rate limiting on _remote_addr
-    let (socket, _remote_addr) = match listener.accept().await {
-    	Ok(s) => s,
-    	Err(_e) => {
-				// log socket error
-    		continue;
-    	},
+    // create config
+    let args = match env::args().nth(1) {
+        Some(a) => path::PathBuf::from(a),
+        None => return println!("argument error:\nconfig params not found."),
     };
-    
-		let io = match tls_acceptor.clone().accept(socket).await {
-			Ok(s) => TokioIo::new(s),
-			Err(_e) => {
-				// log tls error
-				continue;
-			},
-		};
-		
-  	let service = responses::Svc{
-  		addresses: addresses_arc.clone(),
-  	};
-  	
-  	tokio::task::spawn(async move {
-  		// log response error
-  		Builder::new(TokioExecutor::new())
-  			.serve_connection(io, service)
-  			.await
-  	});
-  } 
-}
+    let config = match config::from_filepath(&args) {
+        Ok(c) => c,
+        Err(e) => return println!("configuration error:\n{}", e),
+    };
 
+    // get addresses
+    let host_address = config.host.clone() + ":" + &config.port.to_string();
+    let addresses = match config::create_address_map(&config) {
+        Ok(addrs) => addrs,
+        Err(e) => return println!("address map error:\n{}", e),
+    };
+    let addresses_arc = Arc::new(addresses);
+
+    // tls cert and keys
+    let cert = match fs::read(&config.cert_filepath).await {
+        Ok(f) => f,
+        Err(e) => return println!("cert error:\n{}", e),
+    };
+    let key = match fs::read(&config.key_filepath).await {
+        Ok(f) => f,
+        Err(e) => return println!("cert error:\n{}", e),
+    };
+    let pkcs8 = match Identity::from_pkcs8(&cert, &key) {
+        Ok(pk) => pk,
+        Err(e) => return println!("cert error:\n{}", e),
+    };
+
+    // create tls acceptor
+    let tls_acceptor = match native_tls::TlsAcceptor::builder(pkcs8).build() {
+        Ok(native_acceptor) => tokio_native_tls::TlsAcceptor::from(native_acceptor),
+        Err(e) => return println!("native_acceptor error:\n{}", e),
+    };
+
+    // bind tcp listeners
+    let listener = match TcpListener::bind(host_address).await {
+        Ok(l) => l,
+        Err(e) => return println!("tcp listener error:\n{}", e),
+    };
+
+    // sever loop
+    loop {
+        // rate limiting on _remote_addr
+        let (socket, _remote_addr) = match listener.accept().await {
+            Ok(s) => s,
+            Err(_e) => {
+                // log socket error
+                continue;
+            }
+        };
+
+        let io = match tls_acceptor.clone().accept(socket).await {
+            Ok(s) => TokioIo::new(s),
+            Err(_e) => {
+                // log tls error
+                continue;
+            }
+        };
+
+        let service = responses::Svc {
+            addresses: addresses_arc.clone(),
+        };
+
+        tokio::task::spawn(async move {
+            // log response error
+            Builder::new(TokioExecutor::new())
+                .serve_connection(io, service)
+                .await
+        });
+    }
+}
